2a0c5894180e8c3624124112b6c27294f0a5cc8c
Ruma303 <matteoh8@hotmail.it>
2024-04-27 12:02:22 +0200

    update

--- ts/type.ts
+++ ts/type.ts
@@ -1,9 +1,9 @@
-    (function greet() {
-        let type = "Codice attivato da ts/type.ts";
-        console.log(type);
-        if(app) {
-            app.innerHTML = type;
-        }
-    })();
+(function greet() {
+    let type = "Codice attivato da ts/type.ts";
+    console.log(type);
+    if(app) {
+        app.innerHTML = type;
+    }
+})();
 
 
