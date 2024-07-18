1e7a54894e5c8c54ac64a250f52ca37b85be2060
Ruma303 <matteoh8@hotmail.it>
2024-07-18 17:36:43 +0200

    .

--- index.js
+++ index.js
@@ -50,39 +50,3 @@ var differenza = function (x, y) {
 };
 console.log(somma(5, 3)); // 8
 console.log(differenza(5, 3)); // 2
-var pair1 = { first: 5, second: 10 };
-var pair2 = {
-    prop1: 5,
-    prop2: "hello",
-    prop3: true
-};
-//, Generics functions interface
-function getMiddleElement(items) {
-    if (items.length === 0) {
-        return undefined;
-    }
-    var middleIndex = Math.floor(items.length / 2);
-    return items[middleIndex];
-}
-var numbers = [1, 2, 3, 4, 5];
-var middleNumber = getMiddleElement(numbers);
-console.log("middleNumber: ", middleNumber); // 3
-// middleNumber sarà di tipo number
-var strings = ["apple", "banana", "cherry"];
-var middleString = getMiddleElement(strings);
-console.log("middleString: ", middleString); // banana
-var getArrayLength = function (items) {
-    return items.length;
-};
-var arrLength = getArrayLength([1, 2, 3, 4, 5]);
-//, Più parametri generici
-function pairElements(first, second) {
-    return [first, second];
-}
-var result1 = pairElements(5, "hello");
-console.log(result1); // [5, "hello"]
-var result2 = pairElements(true, { name: "Alice" });
-console.log(result2); // [true, { name: "Alice" }]
-var result3 = pairElements(["apple", "banana"], 42);
-console.log(result3); // [["apple", "banana"], 42]
-//, Bounded Generics
